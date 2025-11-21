70f5d37cf436601842636a84287f1a71e062eb0c
justletmecreatetheaccount <vladdoniga11@gmail.com>
2025-11-22 00:51:22 +0100

    simplifier sanic

--- main.py
+++ main.py
@@ -8,10 +8,6 @@ app.static("/", "./static/index.html", name="index")
 app.static("/plan", "./static/plan.html", name="plan")
 app.static("/draw", "./static/draw.html", name="draw")
 
-app.static("/index.css", "./static/index.css", name="index_css")
-app.static("/draw.css", "./static/draw.css", name="draw_css")
-app.static("/plan.css", "./static/plan.css", name="plan_css")
-
 @app.websocket("/feed")
 async def feed(request: Request, ws: Websocket):
     async for msg in ws:
